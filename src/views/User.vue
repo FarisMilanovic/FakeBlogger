80b8b2e0c61b39f14ee5d36aeadf88ae5de8b6d8
Faris <farismilanovic@icloud.com>
2022-08-02 19:10:07 +0200

    Axios added on user page

--- src/views/User.vue
+++ src/views/User.vue
@@ -1,11 +1,42 @@
 <template>
-  <div class="home">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <input type="search">
+    <div v-for="post in posts" :key="post.id">
+      <h2>{{ post.id }} {{ post.title }}</h2>
+      <p>{{ post.body }}</p>
+  </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
-    name: "HomeView",
+    name: "User",
+    data() {
+      return {
+        posts: []
+      }
+    },
+    mounted() {
+      axios
+        .get('https://jsonplaceholder.typicode.com/posts')
+        .then(response => (this.posts = response.data))
+    },
+
 }
 </script>
+
+<style scoped>
+input{
+  margin-top: 10px;
+  width: 200px;
+  height: 40px;
+  border: 1px solid #333;
+  border-radius: 10px;
+  padding-left: 5px;
+}
+
+
+</style>
+
+
